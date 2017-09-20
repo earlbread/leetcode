46285b26dc0ad3f86f45d69fe125c5a41b0fd2b2
Seunghun Lee <waydi1@gmail.com>
2017-09-20 22:47:55 +0900

    Add Excel Sheet Column Number Python

---
+++ excel-sheet-column-number/excel-sheet-column-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+
+        for c in s:
+            num *= 26
+            num += ord(c) - ord('A') + 1
+
+        return num
