20247f88acbbd044086fe32ab3a9bcac45f52527
Seunghun Lee <waydi1@gmail.com>
2017-12-11 23:19:29 +0900

    Add Sentence Similarity Python

---
+++ sentence-similarity/sentence-similarity.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+class Solution(object):
+    def areSentencesSimilar(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2):
+            return False
+
+        h = defaultdict(list)
+
+        for pair in pairs:
+            h[pair[0]].append(pair[1])
+            h[pair[1]].append(pair[0])
+
+        for word1, word2 in zip(words1, words2):
+            if word1 == word2:
+                continue
+            if h.get(word1) is None or h.get(word2) is None:
+                return False
+            if not (word2 in h.get(word1) or word1 in h.get(word2)):
+                return False
+
+        return True
+
+
+s = Solution()
+
+words1 = ["this","summer","thomas","get","actually","actually","rich","and","possess","the","actually","great","and","fine","vehicle","every","morning","he","drives","one","nice","car","around","one","great","city","to","have","single","super","excellent","super","as","his","brunch","but","he","only","eat","single","few","fine","food","as","some","fruits","he","wants","to","eat","an","unique","and","actually","healthy","life"]
+words2 = ["this","summer","thomas","get","very","very","rich","and","possess","the","very","fine","and","well","car","every","morning","he","drives","a","fine","car","around","unique","great","city","to","take","any","really","wonderful","fruits","as","his","breakfast","but","he","only","drink","an","few","excellent","breakfast","as","a","super","he","wants","to","drink","the","some","and","extremely","healthy","life"]
+pairs = [["good","nice"],["good","excellent"],["good","well"],["good","great"],["fine","nice"],["fine","excellent"],["fine","well"],["fine","great"],["wonderful","nice"],["wonderful","excellent"],["wonderful","well"],["wonderful","great"],["extraordinary","nice"],["extraordinary","excellent"],["extraordinary","well"],["extraordinary","great"],["one","a"],["one","an"],["one","unique"],["one","any"],["single","a"],["single","an"],["single","unique"],["single","any"],["the","a"],["the","an"],["the","unique"],["the","any"],["some","a"],["some","an"],["some","unique"],["some","any"],["car","vehicle"],["car","automobile"],["car","truck"],["auto","vehicle"],["auto","automobile"],["auto","truck"],["wagon","vehicle"],["wagon","automobile"],["wagon","truck"],["have","take"],["have","drink"],["eat","take"],["eat","drink"],["entertain","take"],["entertain","drink"],["meal","lunch"],["meal","dinner"],["meal","breakfast"],["meal","fruits"],["super","lunch"],["super","dinner"],["super","breakfast"],["super","fruits"],["food","lunch"],["food","dinner"],["food","breakfast"],["food","fruits"],["brunch","lunch"],["brunch","dinner"],["brunch","breakfast"],["brunch","fruits"],["own","have"],["own","possess"],["keep","have"],["keep","possess"],["very","super"],["very","actually"],["really","super"],["really","actually"],["extremely","super"],["extremely","actually"]]
+
+print(s.areSentencesSimilar(words1, words2, pairs))
+
+words1 = ["great","acting","skills"]
+words2 = ["fine","painting","talent"]
+pairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+print(s.areSentencesSimilar(words1, words2, pairs))
